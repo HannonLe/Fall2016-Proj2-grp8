5c2b01f56788bd01b2ad2e6b162bc10c9002b0ce
Wanyi Zhang <mushroomvv@gmail.com>
2016-10-10 13:14:34 -0400

    Add files via upload
    R scripts for creating time-series-table.csv

---
+++ lib/create-time-series-table.R
@@ -0,0 +1,22 @@
+#For time series plot
+
+require(readr)
+require(dplyr)
+
+noise <- read_csv("~/Google Drive/Columbia/5243 ADS/Project 2/Fall2016-Proj2-grp8/data/Cleaned-311-Noise-Date.csv",col_names=T)
+df=data.frame(table(noise$Date))
+names(df)<-c("Date","Freq")
+
+# add columns of Months, Weeks, and Weekdays (Day)
+df$Date <- as.Date(df$Date,format="%m/%d/%y") #convert to standard Date format
+df$Day <- format(df$Date,"%a") #Add Day column
+df$Month <- as.Date(cut(df$Date,breaks = "month"))
+df <- mutate(df,Month=substr(Month,6,7)) #Add Month column
+df$Week <- as.Date(cut(df$Date,breaks = "week",start.on.monday = T)) # Add Week column
+
+write.table(df,file="time-series-table.csv")
+list.files()
+
+WeekMean=summarise(group_by(df,Week),round(mean(Freq)))
+DayMean=summarise(group_by(df,Day),round(mean(Freq)))
+
